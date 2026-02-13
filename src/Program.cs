ee5776ee51b6a189b727ccfb3884008f1d6d3ab7
PaTuan <tuantuan230298@gmail.com>
2026-02-14 02:00:33 +0700

    commit

--- src/Program.cs
+++ src/Program.cs
@@ -21,12 +21,6 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseForwardedHeaders();
 
